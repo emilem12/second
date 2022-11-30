fb3a61f1f280ad1759652b2c572878697a0fa0e5
Emil <mawlitov.e@yandex.ru>
2022-11-30 23:12:38 +0400

    fdfd

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿Console.WriteLine("Введите превое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите третье число");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
